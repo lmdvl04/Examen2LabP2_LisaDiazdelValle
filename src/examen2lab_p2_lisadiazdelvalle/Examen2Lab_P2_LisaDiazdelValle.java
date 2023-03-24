5798915aa503d759eb3da8a8ad612fd86247d557
Lisa Diaz del Valle <lmdvl04@gmail.com>
2023-03-24 13:57:14 -0600

    principal

---
+++ src/examen2lab_p2_lisadiazdelvalle/Examen2Lab_P2_LisaDiazdelValle.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen2lab_p2_lisadiazdelvalle;
+
+/**
+ *
+ * @author HTS
+ */
+public class Examen2Lab_P2_LisaDiazdelValle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
